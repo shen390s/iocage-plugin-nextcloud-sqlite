0b7013984527f02ca64f4817518b797bebfe224d
Rongsong Shen <shenrs@cn.ibm.com>
2020-12-18 11:36:23 +0800

    reshape directory

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,5 @@
 #!/bin/sh
 
 if [ -f /root/bin/apply_role.sh ]; then
-   sh /root/bin/apply_role.sh jails/nextcloud-sqlite setup
+   sh /root/bin/apply_role.sh jails/cloud/nextcloud-sqlite setup
 fi
